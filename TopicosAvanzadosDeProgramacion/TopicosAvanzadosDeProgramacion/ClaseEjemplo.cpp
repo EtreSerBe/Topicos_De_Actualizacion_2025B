c6542c34a1063f025cfde8a96033ba1e20bcd83b
Adrian Gonzalez <agonzalez@titangames.com.mx>
2025-06-12 09:01:22 -0600

    branches ejemplo

---
+++ TopicosAvanzadosDeProgramacion/TopicosAvanzadosDeProgramacion/ClaseEjemplo.cpp
@@ -0,0 +1,21 @@
+
+
+class Alumno
+{
+	int Progra, Dibujo, Legal;
+};
+
+// adrian: función que calcule, asigne e imprima la calificación de progra
+
+
+// Juanito: función que calcule, asigne e imprima la calificación de Dibujo
+
+
+int MainEjemplo()
+{
+
+	return 0;
+
+}
+
+
