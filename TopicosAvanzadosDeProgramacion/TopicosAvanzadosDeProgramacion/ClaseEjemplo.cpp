c6d7987d9707f26e85347b3a6c83b17c4415a7a9
Adrian Gonzalez <agonzalez@titangames.com.mx>
2025-06-12 09:09:42 -0600

    cambios adrian

--- TopicosAvanzadosDeProgramacion/TopicosAvanzadosDeProgramacion/ClaseEjemplo.cpp
+++ TopicosAvanzadosDeProgramacion/TopicosAvanzadosDeProgramacion/ClaseEjemplo.cpp
@@ -5,14 +5,13 @@ class Alumno
 	int Progra, Dibujo, Legal;
 };
 
-// adrian: función que calcule, asigne e imprima la calificación de progra
-
-
-// Juanito: función que calcule, asigne e imprima la calificación de Dibujo
 
 
 int MainEjemplo()
 {
+
+	
+
 
 	return 0;
 
---
+++ TopicosAvanzadosDeProgramacion/TopicosAvanzadosDeProgramacion/ClaseEjemploBranchAdrian.cpp
@@ -0,0 +1,33 @@
+
+
+class Alumno
+{
+public:
+	int Progra, Dibujo, Legal;
+};
+
+// adrian: función que calcule, asigne e imprima la calificación de progra
+
+
+// Juanito: función que calcule, asigne e imprima la calificación de Dibujo
+
+void CalcularAsignarEImprimirCaliDeProgra(Alumno& alumno)
+{
+	// cálculos y demás
+	alumno.Progra = 100;
+
+}
+
+int MainEjemplo()
+{
+	Alumno Pepito;
+
+
+	CalcularAsignarEImprimirCaliDeProgra(Pepito);
+
+
+	return 0;
+
+}
+
+
